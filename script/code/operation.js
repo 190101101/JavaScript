8635017a82e96c542e2b8f0f5c91c56c26ba589a
190101101 <190101101@mail.ru>
2023-10-31 07:33:03 +0400

    10 number date timer\192 Intern of Date

--- script/code/operation.js
+++ script/code/operation.js
@@ -6,11 +6,11 @@ console.log(Number(futureDate));
 console.log(new Date(Number(futureDate)));
 
 const getDaysBetween2Dates = (date1, date2) =>
-  (date2 - date1) / (1000 * 60 * 60 * 24);
+    Math.abs((date1 - date2) / (1000 * 60 * 60 * 24));
 
 const getDate = getDaysBetween2Dates(
   new Date(1991, 7, 19),
-  new Date(2023, 10, 30)
+  new Date(2023, 10, 30),
 );
 
 console.log(getDate);
