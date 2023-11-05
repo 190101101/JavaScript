7d47373e77f41e510a2b4d35649f7eb305e7bcfe
190101101 <190101101@mail.ru>
2023-11-05 17:48:33 +0400

    repeat\11\206 Event Delegation

---
+++ script/note/task.js
@@ -0,0 +1,11 @@
+//TODO 206 Event Propagation (Распространение событий);
+//? currentTarget zencir vari parente qeder gedir.
+//? target yalniz click olana aiddir
+//? Propagation dayandirmaq ucun e.stopPropagation()
+//? currentTarget === this => true
+//? EventListener - 3 cu param boolean qebul edir. eger true ise 1 ci faza da olur default olarak false olur. amma bu param artik kohnedir.
+
+//Todo: 207 Event Delegation (делегирования событий);
+//? 1. ümumi parent ucun event listener elave et.
+//? 3. eget target olunmush element nav__link deyilse false
+//? 3. target olunmush elementi mueyyen et
